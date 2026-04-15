389a1d02ad6e713f4b9f76dcf811625d055f7cd8
erickjdslog-code <erickjdslog@gmail.com>
2026-04-15 22:23:37 +0000

    cambios generador

--- generador.js
+++ generador.js
@@ -1,6 +1,6 @@
-let pronoun = ['the', 'our'];
-let adj = ['great', 'big'];
-let noun = ['jogger', 'racoon'];
+let pronoun = ['the', 'our', 'this'];
+let adj = ['great', 'big', 'awesome'];
+let noun = ['jogger', 'racoon', 'ball'];
 
 for (let i = 0; i < pronoun.length; i++) {
     for (let j = 0; j < adj.length; j++) {
